4567db114e771d788f5746d0fc9dfb6f9255f4b1
Hartman- <thehartman8@gmail.com>
2020-10-02 15:26:29 -0400

    adding more exception handling, simple diff check for spelling

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# Hello World
+# **DnD5e Discord Bot**
+
+Playing around with Discord Bots and API calls. 
+
+## Currently Implemented
+- *!roll* : Simple Dice Roller 
+- *!spell* : Spell Lookup
+
+**All commands bound to the '!' character*
+
+### **Dice Roller** *!roll*
+
+### **Spell Lookup** *!spell*
+Very rough, currently only basic exception handling with requiring exact spelling. Plans to add a system to suggest similar/close spells to what was typed. 
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # main.py
+import difflib
 import os
 import random
 import re
@@ -12,8 +13,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN = os.getenv('DISCORD_BOT_TOKEN')
-
 bot = commands.Bot(command_prefix="!")
+
 
 @bot.event
 async def on_ready():
@@ -44,14 +45,19 @@ async def roll_dice(ctx, dice_and_mod: str):
             for _ in range (num_dice)
         ]
 
-        # add modifier to each roll
-        roll_mod = [x+mod for x in roll_base]
+        # sum rolls
+        # add modifier
+        roll_sum = sum(roll_base) + mod
 
         # convert lists to strings
-        roll_base_str = ', '.join(str(x) for x in roll_base)
-        roll_mod_str = ', '.join(str(x) for x in roll_mod)
+        roll_base_str = ','.join(str(x) for x in roll_base)
         
-        msg_roll = "Rolled: {0}\nResult: {1}\nWith Modifier: {2}".format(dice_and_mod, roll_base_str, roll_mod_str)
+        msg_roll = "**{}**\n{:20}{:>15}\n{:20}**{:>15}**".format(
+            dice_and_mod,
+            "Dice Rolls:",
+            roll_base_str,
+            "Result:",
+            roll_sum)
         await ctx.send(msg_roll)
 
     else:
@@ -59,54 +65,126 @@ async def roll_dice(ctx, dice_and_mod: str):
 
 
 # thanks stack overflow
-def find_match(json_obj, name):
+# def find_match(json_obj, name):
+#     """
+#     Given a JSON object and a name
+#     return the matching index value, assuming no duplicates
+#     """
+#     return [obj for obj in json_obj if obj['name'].lower() == name][0]['index']
+
+
+# Given two strings, do a simple diff check 
+# return strings within given error margin
+def check_name(search, name):
+    error_margin = 5
+    if search == name:
+        return [True, name]
+    else:
+        diff_list = [li for li in difflib.ndiff(search, name) if li[0] != ' ']
+        if len(diff_list) <= error_margin:
+            return [True, name]
+        return [False, name]
+
+
+# wrapper for requests that need the same basic exception handling
+def get_request(url):
     """
-    Given a JSON object and a name
-    return the matching index value, assuming no duplicates
+    Handle connection / timeout / HTTP errors together
+    Exit and print message if there is a problem
     """
-    return [obj for obj in json_obj if obj['name'].lower() == name][0]['index']
+    try:
+        # api request
+        response = requests.get(url)
+    
+    except requests.exceptions.RequestException as e:
+        print("Error when trying to connect to the DnD5e API. Please try again shortly.")
+        print(e) #print error to code terminal for debugging
+        return None
+
+    return response
 
 
 @bot.command(name="spell")
 async def search_spell(ctx, *args):
     api_baseUrl = 'https://www.dnd5eapi.co/api/'
-
-    arg = ' '.join(args)
-    search = arg.lower()
-
-    # api request for all spells to iterate and search
-    r_allSpells = requests.get(api_baseUrl+'spells')
-
+    r_allSpells = get_request(api_baseUrl+'spells')
+    
+    # Continue if not None
     if r_allSpells:
         allSpells_json = r_allSpells.json()
         spells = allSpells_json['results']
-
+        
         # get api index
-        spellIndex = find_match(spells, search)
+        arg = ' '.join(args)
+        search = arg.lower()
+        spellIndex = None
+        #spellIndex = find_match(spells, search)
+
+        # Check for similar spell names if no spell name exact match is found
+        # kick out of the loop and send a message to the user to try a valid input
+        close_names = []
+        for spell in spells:
+            spellName = spell['name'].lower()
+            if spellName == search:
+                spellIndex = spell['index']
+
+            else:
+                isCloseMatch = check_name(search, spellName)
+                if isCloseMatch[0]:
+                    close_names.append(spellName)
 
         # as long as we get a non-None return value, we gud
         if spellIndex:
             spellUrl = api_baseUrl+'spells/'+spellIndex
-            r_searchSpell = requests.get(spellUrl)
+            r_searchSpell = get_request(spellUrl)
 
-            spell_levels = ['cantrip', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-            spell = r_searchSpell.json()
+            if r_searchSpell:
+                spell_levels = ['cantrip',
+                    '1st Level',
+                    '2nd Level',
+                    '3rd Level',
+                    '4th Level',
+                    '5th Level',
+                    '6th Level',
+                    '7th Level',
+                    '8th Level',
+                    '9th Level']
+                spell = r_searchSpell.json()
 
-            # simple message formatting with discord markdown
-            msg = "**{}** | `{} {}{}`\nCasting Time: {}\nRange: {}\nComponents: {} {}\nDuration: {}{}\n```{}```".format(
-                spell['name'],
-                spell['school']['name'],
-                spell_levels[spell['level']],
-                ', Ritual' if spell['ritual'] else '',
-                spell['casting_time'],
-                spell['range'],
-                ', '.join(spell['components']),
-                '('+spell['material']+')' if 'material' in spell else '',
-                'Concentration, ' if spell['concentration'] else '',
-                spell['duration'],
-                ''.join(spell['desc']))
+                # simple message formatting with dscord markdown
+                msg = "**{}** | `{}, {}{}`\nCasting Time: {}\nRange: {}\nComponents: {} {}\nDuration: {}{}\n```{}```".format(
+                    spell['name'],
+                    spell['school']['name'],
+                    spell_levels[spell['level']],
+                    ', Ritual' if spell['ritual'] else '',
+                    spell['casting_time'],
+                    spell['range'],
+                    ', '.join(spell['components']),
+                    '('+spell['material']+')' if 'material' in spell else '',
+                    'Concentration, ' if spell['concentration'] else '',
+                    spell['duration'],
+                    ''.join(spell['desc']))
 
-            await ctx.send(msg)
+                await ctx.send(msg)
+            
+            # Basic handling of a None/null response to the request
+            else:
+                await ctx.send('Error when trying to connect to the DnD5e API. Please try again shortly.')
+                return
+        
+        # This case should never happen but it's possible I guess
+        else:
+            # if we found a close result
+            if len(close_names) > 0:
+                await ctx.send("Did you mean one of the following commands: *!spell {}*?".format('* / *!spell '.join(close_names)))
+                return
+            await ctx.send("Looks like I couldn't find a spell by the name [**{}**]. Please try again.".format(search))
+            return
+    
+    # Final basic handling of a None/null response to the request
+    else:
+        await ctx.send('Error when trying to connect to the DnD5e API. Please try again shortly.')
+        return
 
 
 bot.run(TOKEN)
